c82388536e699a70cc3e23b76d6a6738bfcbdf11
tanakanat <50547546+tanakanat@users.noreply.github.com>
2022-04-26 01:36:45 +0900

    bootstrap-vue の導入

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.css' // add
+import 'bootstrap-vue/dist/bootstrap-vue.css' // add
+
 Vue.config.productionTip = false
 
 new Vue({

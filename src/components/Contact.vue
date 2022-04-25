e3ffa992a48c35e86456d69b3875fe56eeb0ce21
tanakanat <50547546+tanakanat@users.noreply.github.com>
2022-04-26 01:09:25 +0900

    テンプレートに画面の要素を記載

---
+++ src/components/Contact.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>お問い合わせフォーム</h1>
+    <div>会社名</div>
+    <div>担当者名</div>
+    <div>メールアドレス</div>
+    <div>お問い合わせ分類</div>
+    <div>契約番号</div>
+    <div>お問い合わせ要件</div>
+    <button>送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
+
+<style scoped>
+</style>
